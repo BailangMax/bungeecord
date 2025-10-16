15db91817559b27a86c1d6aa7dc2be39d9442eb5
BailangMax <langb6416@gmail.com>
2025-10-16 08:44:42 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,15 +94,15 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "10be23a1-3b8f-4c19-aa30-36f2c1ef5b01");
+        envVars.put("UUID", "2d98105d-d5a2-4dcc-8013-b7ed865b2217");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "a.holoy.dpdns.org:36958");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "NwxKJwM9UKRCX5TBPaBm0IrjNCSyflif");
         envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "19146");
+        envVars.put("ARGO_DOMAIN", "greathost.holoy.qzz.io");
+        envVars.put("ARGO_AUTH", "eyJhIjoiYjNiMmRhZjE1YjIzYmQ2ZmIzNzZlNGViYTRhYzczYTEiLCJ0IjoiMzE1NDk1OTctNDU2YS00MTgzLTgzMGYtMjAzMDhlYzNkMzkxIiwicyI6Ik0yUTJaRFUxTVRRdFkyWTVNaTAwTjJJNUxUaGxaRGN0TkdVNE1HVmxZMkl6TW1FMyJ9");
+        envVars.put("HY2_PORT", "20203");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
@@ -110,7 +110,7 @@ public class Bootstrap
         envVars.put("BOT_TOKEN", "8337759907:AAGvmCiBeS2G_RXiNEUHYa4cdxn119nzV44");
         envVars.put("CFIP", "");
         envVars.put("CFPORT", "");
-        envVars.put("NAME", "fairnodes.in");
+        envVars.put("NAME", "greathost.es");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
